e4618a389e6b8ec9dd981ddfce2b74a6d90d452e
Irene Robb <IreneRobb@Irenes-MacBook-Pro.local>
2016-02-03 14:42:22 +1300

    sound workin

--- app/router.js
+++ app/router.js
@@ -12,9 +12,8 @@ export class Router {
         var circle = $(e.target)
         circle.css('fill', 'blue')
         var audioElement = document.createElement('audio')
-        audioElement.setAttribute('src', 'audio/Dank Soundboard 1.app')
+        audioElement.setAttribute('src', 'audio/fettywap.mp3')
         audioElement.setAttribute('autoplay', 'autoplay')
-
         $.get()
         audioElement.addEventListener("load", function() {
           audioElement.play()
