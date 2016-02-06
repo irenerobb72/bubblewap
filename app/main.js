1361ee826b74e7272f7437c5685772d3caf2affc
irene robb <email": null>
2016-02-07 11:38:22 +1300

    checking audio on laptop

--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
 import $ from 'jquery'
 
-  $(() => {
-    let that = this
-    $('.circles').click(function(e) {
-      var circle = $(e.target)
-      circle.css('fill', '#FFD700')
-  })
+$(() => {
+  let that = this
+  $('.circles').click(function(e) {
+    var circle = $(e.target)
+    circle.css('fill', '#A020F0')
 
+    var audioElement = document.createElement('audio')
+      audioElement.setAttribute('src', 'audio/fettywap.mp3')
+      audioElement.setAttribute('autoplay', 'autoplay')
+      $.get()
+      audioElement.addEventListener("load", function() {
+        audioElement.Play()
+    }, true)
+  })
 })
