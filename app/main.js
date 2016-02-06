c0d550b0faf2ebc15dfb871e0a44b99274b17019
irene robb <email": null>
2016-02-07 11:30:40 +1300

    yay colors

--- app/main.js
+++ app/main.js
@@ -1,14 +1,10 @@
 import $ from 'jquery'
 
-function listen() {
-  let that = this
   $(() => {
+    let that = this
     $('.circles').click(function(e) {
-      console.log(e)
       var circle = $(e.target)
       circle.css('fill', '#FFD700')
-    })
   })
-  span = document.getElementById("thecircles")
-  span.innerHTML = circles
-}
+
+})
