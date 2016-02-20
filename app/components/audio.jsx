c16eb81712d32921203f9d7bdc52adb628112ad3
Irene Robb <irenerobb72@gmail.com>
2016-02-20 20:27:26 +1300

    adding unworking audio

--- app/components/audio.jsx
+++ app/components/audio.jsx
@@ -7,6 +7,15 @@ export class Audio extends Component {
   constructor(props) {
     super(props)
   }
+  audioElement() {
+    document.createElement('audio')
+      audioElement.setAttribute('src', 'audio/fettywap.mp3')
+      audioElement.setAttribute('autoplay', 'autoplay')
+      $.get()
+      audioElement.addEventListener("load", function() {
+        audioElement.Play()
+    }, true)
+  }
   render() {
     return (
 
--- app/components/event.jsx
+++ app/components/event.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import reactMixin from 'react-mixin'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { connect } from 'react-redux'
+import { Audio } from 'audio'
 
 export class Event extends Component {
   constructor(props) {
@@ -9,7 +10,8 @@ export class Event extends Component {
   }
   handleClick(e) {
     e.preventDefault()
-
+    var circle = $(e.target)
+    <Audio />
   }
   render() {
 
